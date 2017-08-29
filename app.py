ba4afce0d2f8d4d37a5eb4a8be2292b8e71a5eed
James Ives <iam@jamesiv.es>
2017-08-28 19:35:32 -0500

    Removing Interval Timer Adjustment

--- app.py
+++ app.py
@@ -177,12 +177,9 @@ if __name__ == "__main__":
     if slack_client.rtm_connect():
 
         # Check if interval checking is setup, otherwise inform the user.
-        if CHARTBEAT_ENDPOINT != '' and CHARTBEAT_OUTPUT_CHANNEL != '' and CHARTBEAT_INTERVAL_TIME != '':
+        if CHARTBEAT_ENDPOINT != '' and CHARTBEAT_OUTPUT_CHANNEL != '':
 
-            if CHARTBEAT_INTERVAL_TIME == '':
-                CHARTBEAT_INTERVAL_TIME = '0'
-
-            schedule.every(int(CHARTBEAT_INTERVAL_TIME)).minutes.do(validate_chartbeat_schedule)
+            schedule.every(60).minutes.do(validate_chartbeat_schedule)
 
         else: 
             print('Chartbeat data cannot be found, please refer to the documentation.')
